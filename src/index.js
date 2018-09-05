7db52dbf3da3efd9c583cbebfea03e9eeec48c34
gglin26594 <lin.cityu@gmail.com>
2018-09-05 14:13:02 +1200

    apply dev tools: redux-logger, redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 import { Provider } from "react-redux";
-import {createStore} from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { logger } from "redux-logger";
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	document.getElementById("root")
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
 registerServiceWorker();
