b2c2cc00edf52b1d86999c0ef4158314c4969185
gglin26594 <lin.cityu@gmail.com>
2018-09-05 14:01:50 +1200

    set up provider, reducers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from "react-redux";
+import {createStore} from "redux"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore();
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+);
 registerServiceWorker();
