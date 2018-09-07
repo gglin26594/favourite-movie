dc11abfd1ff4183bb26b009f9696f7f758f14294
gglin26594 <lin.cityu@gmail.com>
2018-09-07 12:45:43 +1200

    add frond-end validation to check empty input of movie form

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -1,25 +1,46 @@
 import React, { Component } from "react";
+import classnames from "classnames";
 
 class MovieForm extends Component {
   state = {
     title: "",
     cover: "",
-    error: {}
+    errors: {}
   };
 
   handleOnChange = e => {
+    if (!!this.state.errors[e.target.name]) {
+      if (e.target.value !== "") {
+        let errors = Object.assign({}, this.state.errors);
+        delete errors[e.target.name];
+        this.setState({ errors });
+      }
+    } 
     this.setState({ [e.target.name]: e.target.value });
   };
 
   handleSubmit(e) {
     e.preventDefault();
+
+    let errors = {};
+    if (this.state.title === "") {
+      errors.title = "Movie title can NOT be empty.";
+    }
+    if (this.state.cover === "") {
+      errors.cover = "Cover URL can NOT be empty.";
+    }
+    this.setState({ errors });
   }
   render() {
     return (
       <div>
         <form className="ui form">
           <h1>Add a new movie</h1>
-          <div className="field">
+          <div
+            className={classnames("field", {
+              error: !!this.state.errors.title
+            })}
+          >
             <label>Movie Title</label>
             <input
               value={this.state.title}
@@ -28,8 +49,13 @@ class MovieForm extends Component {
               name="title"
               placeholder="Movie Title"
             />
+            <span>{this.state.errors.title}</span>
           </div>
-          <div className="field">
+          <div
+            className={classnames("field", {
+              error: !!this.state.errors.cover
+            })}
+          >
             <label>Cover Image URL</label>
             <input
               value={this.state.cover}
@@ -38,6 +64,7 @@ class MovieForm extends Component {
               name="cover"
               placeholder="Cover Image URL"
             />
+            <span>{this.state.errors.cover}</span>
           </div>
           <div className="field">
             {this.state.cover !== "" && (
